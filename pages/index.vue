f11818ad57de6342a8446c3c8a77af23c76aa84d
rmorioka <morioka@feedforce.jp>
2020-03-02 20:31:13 +0900

    write all parametors in store

--- pages/index.vue
+++ pages/index.vue
@@ -8,40 +8,43 @@
         ref="canvas"
         :width="mainWidth"
         :height="mainHeight"
-        :rect-size="rectSize"
-        :color-red="colorRed"
-        :color-green="colorGreen"
-        :blue-speed="blueSpeed"
+        :rect-size="rectSize.value"
+        :color-red="colorRed.value"
+        :color-green="colorGreen.value"
+        :blue-speed="blueSpeed.value"
       ></base-canvas>
     </div>
     <div class="controller">
       <controll-slider
         slider-name="rectSize"
-        :min-value="10"
-        :max-value="100"
-        :value="rectSize"
-        unit-name="px"
+        :min-value="rectSize.minValue"
+        :max-value="rectSize.maxValue"
+        :value="rectSize.value"
+        :unit-name="rectSize.unitName"
         @updateValue="updateRectSize"
       ></controll-slider>
       <controll-slider
         slider-name="colorRed"
-        :min-value="0"
-        :max-value="255"
-        :value="colorRed"
+        :min-value="colorRed.minValue"
+        :max-value="colorRed.maxValue"
+        :value="colorRed.value"
+        :unit-name="colorRed.unitName"
         @updateValue="updateColorRed"
       ></controll-slider>
       <controll-slider
         slider-name="colorGreen"
-        :min-value="0"
-        :max-value="255"
-        :value="colorGreen"
+        :min-value="colorGreen.minValue"
+        :max-value="colorGreen.maxValue"
+        :value="colorGreen.value"
+        :unit-name="colorGreen.unitName"
         @updateValue="updateColorGreen"
       ></controll-slider>
       <controll-slider
         slider-name="blueSpeed"
-        :min-value="1"
-        :max-value="10"
-        :value="blueSpeed"
+        :min-value="blueSpeed.minValue"
+        :max-value="blueSpeed.maxValue"
+        :value="blueSpeed.value"
+        :unit-name="blueSpeed.unitName"
         @updateValue="updateBlueSpeed"
       ></controll-slider>
     </div>
--- store/index.js
+++ store/index.js
@@ -1,23 +1,43 @@
 export const state = () => ({
   canvasVariables: {
-    rectSize: 50,
-    colorRed: 127,
-    colorGreen: 127,
-    blueSpeed: 1
+    rectSize: {
+      value: 50,
+      minValue: 10,
+      maxValue: 100,
+      unitName: 'px'
+    },
+    colorRed: {
+      value: 127,
+      minValue: 0,
+      maxValue: 255,
+      unitName: ''
+    },
+    colorGreen: {
+      value: 127,
+      minValue: 0,
+      maxValue: 255,
+      unitName: ''
+    },
+    blueSpeed: {
+      value: 1,
+      minValue: 1,
+      maxValue: 10,
+      unitName: ''
+    }
   }
 })
 
 export const mutations = {
   updateRectSize(state, value) {
-    state.canvasVariables.rectSize = value
+    state.canvasVariables.rectSize.value = value
   },
   updateColorRed(state, value) {
-    state.canvasVariables.colorRed = value
+    state.canvasVariables.colorRed.value = value
   },
   updateColorGreen(state, value) {
-    state.canvasVariables.colorGreen = value
+    state.canvasVariables.colorGreen.value = value
   },
   updateBlueSpeed(state, value) {
-    state.canvasVariables.blueSpeed = value
+    state.canvasVariables.blueSpeed.value = value
   }
 }
